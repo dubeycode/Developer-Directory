fcf4d68504ba012e5b59e3cffb61bac301db11ee
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-02 19:41:58 +0530

    Mongodb connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -7,9 +8,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoUrl = process.env.MONGODB_URL;
+
+try {
+  await mongoose.connect(mongoUrl);
+  console.log('Connected to MongoDB');
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error);
+}
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Developer Directory Backend!');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
